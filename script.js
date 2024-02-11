8efc6c833c2b8680b60f09fe5d86f9345b5d299a
EbrarYasliPXL <12201323@student.pxl.be>
2024-02-11 15:04:26 +0100

    test recaptcha

--- script.js
+++ script.js
@@ -23,12 +23,30 @@ function openmenu(){
     sidemenu.style.right = "-200px";
 }
 //contact script
-function sendMail(){
-    let parms = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        message : document.getElementById("message").value,
-    }
+function sendMail() {
+    // Haal de waarde van de reCAPTCHA op
+    var recaptchaResponse = grecaptcha.getResponse();
 
-    emailjs.send("service_ktfdyhn", "template_g8bfznw", parms).then(alert("Email Sent!"))
+    // Controleer of alle velden zijn ingevuld en reCAPTCHA is voltooid
+    if (recaptchaResponse !== "" &&
+        document.getElementById("name").value.trim() !== "" &&
+        document.getElementById("email").value.trim() !== "" &&
+        document.getElementById("message").value.trim() !== "") {
+
+        // Maak het parms-object
+        let parms = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            message: document.getElementById("message").value,
+        };
+
+        // Verstuur de e-mail met emailjs
+        emailjs.send("service_ktfdyhn", "template_g8bfznw", parms).then(function () {
+            alert("Email Sent!");
+        });
+
+    } else {
+        // Toon een foutmelding als niet alle velden zijn ingevuld of reCAPTCHA niet is voltooid
+        alert("Vul alle velden in en voltooi de robot test.");
+    }
 }
