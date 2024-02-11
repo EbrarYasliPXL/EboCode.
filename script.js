9c090f3448b295a666c5359f715d54610d5622d1
EbrarYasliPXL <12201323@student.pxl.be>
2024-02-11 20:04:52 +0100

    cookie beleid midden pagina

--- script.js
+++ script.js
@@ -63,30 +63,48 @@ function handleRecaptcha(response) {
 }
 
 //cookies script
-setCookie = (cName, cValue, expDays) => {
+const checkCookie = () => {
+    return getCookie("cookie") === "true";
+};
+
+const setCookie = (cName, cValue, expDays) => {
     let date = new Date();
-    date.setTime((date.getTime() + expDays * 24 * 60 * 60 * 1000));
+    date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
     const expires = "expires=" + date.toUTCString();
     document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
-}
+};
 
-getCookie = (cName) => {
+const getCookie = (cName) => {
     const name = cName + "=";
     const cDecoded = decodeURIComponent(document.cookie);
     const cArr = cDecoded.split("; ");
     let value;
     cArr.forEach(val => {
-        if(val.indexOf(name) === 0) value = val.substring(name.length);
-    })
+        if (val.indexOf(name) === 0) value = val.substring(name.length);
+    });
     return value;
-}
-document.querySelector("#cookies-btn").addEventListener("click", () => {
-    document.querySelector("#cookies").style.display = "none";
-    setCookie("cookie", true, 30)
-})
+};
 
-cookieMessage = () => {
-    if (!getCookie("cookie"))
+const showCookieMessage = () => {
+    if (!checkCookie()) {
         document.querySelector("#cookies").style.display = "block";
-}
-window.addEventListener("load", cookieMessage);
+        // Disable scrolling
+        document.body.style.overflow = "hidden";
+    }
+};
+
+const hideCookieMessage = () => {
+    document.querySelector("#cookies").style.display = "none";
+    // Enable scrolling
+    document.body.style.overflow = "auto";
+};
+
+const acceptCookies = () => {
+    hideCookieMessage();
+    setCookie("cookie", true, 30);
+};
+
+document.querySelector("#cookies-btn").addEventListener("click", acceptCookies);
+
+// Show cookie message on page load
+window.addEventListener("load", showCookieMessage);
